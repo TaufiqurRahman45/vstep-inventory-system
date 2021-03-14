6a25c5f8edfeacbae6a3636447b3ab91411e82eb
Md Taufiqur Rahman <toufiqurrahman45@gmail.com>
2021-03-14 22:20:09 +0800

    inventory system

---
+++ inventory management system/inventory/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from store.models import Product, Supplier, Order
+
+@login_required(login_url='login')
+def dashboard(request):
+    total_product = Product.objects.count()
+    total_supplier = Supplier.objects.count()
+    total_oder = Order.objects.count()
+    orders = Order.objects.all().order_by('-id')
+    context = {
+        'product': total_product,
+        'supplier': total_supplier,
+        'order': total_oder,
+        'orders': orders
+    }
+    return render(request, 'dashboard.html', context)
+    
---
+++ inventory management system/store/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from .models import (
+    Supplier,
+    Purchaseorder,
+    Season,
+    Drop,
+    Product,
+    Order,
+    Delivery
+)
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'address', 'created_date']
+
+class PurchaseorderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'address', 'created_date']
+
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(Purchaseorder, PurchaseorderAdmin)
+admin.site.register(Season)
+admin.site.register(Drop)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Delivery)
