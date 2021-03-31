cc4e3b06a60f689025b2f4e07f551e7138346c76
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-03-31 19:55:35 +0600

    Add custom field to useradmin

--- inventory management system/users/admin.py
+++ inventory management system/users/admin.py
@@ -6,6 +6,37 @@ from .models import User
 
 class UserAdmin(BaseUserAdmin):
     list_display = ['username', 'is_operation', 'is_admin', 'is_ppc']
+    fieldsets = (
+        ('User', {'fields': ('username', 'password')}),
+        ('Personal Information', {'fields': (
+            'first_name',
+            'last_name',
+            'email',
+        )}),
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'is_operation',
+            'is_admin',
+            'is_ppc',
+        )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username',
+                'password1',
+                'password2',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'is_operation',
+                'is_admin',
+                'is_ppc',),
+        }),
+    )
 
 
 admin.site.register(User, UserAdmin)
