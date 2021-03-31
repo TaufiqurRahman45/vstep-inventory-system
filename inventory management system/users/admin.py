b56791218cae44aa9cd9c59765042321b79410e4
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-03-31 19:40:34 +0600

    Fixed admin plain password

--- inventory management system/users/admin.py
+++ inventory management system/users/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ['username', 'is_operation', 'is_admin', 'is_ppc']
 
 
